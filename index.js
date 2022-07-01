581155e14f0dc4484d419793dfff0a8b6d905553
munashe <munashemugondaa@gmail.com>
2022-07-01 08:34:50 +0200

    stack

---
+++ index.js
@@ -0,0 +1,34 @@
+class Node{
+    constructor(val){
+        this.val = val 
+        this.next = null
+    }
+}
+
+class Stack {
+    constructor() {
+        this.first = null 
+        this.last = null 
+        this.size = 0
+    }
+
+    push(val) {
+        let newNode = new Node(val)
+        if(!this.first) {
+            this.first = newNode 
+            this.last = newNode
+        }else {
+            var temp = this.first 
+            this.first = newNode  
+            this.first.next = temp
+        }
+         this.size++
+        return this
+    }
+}
+
+
+let heap = new Stack() 
+heap.push("munashe")
+heap.push("madhuna")
+console.log(heap)
